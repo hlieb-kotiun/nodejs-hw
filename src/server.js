1c5b6ca0277cfe658b410cd8f54e7ebb04cd4d6e
hlieb-kotiun <gleb.kotiun@gmail.com>
2026-06-23 19:55:54 +0200

    hm-1

--- src/server.js
+++ src/server.js
@@ -1,1 +1,60 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import pino from 'pino-http';
 
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message: `${error.message}`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend run on Port : ${PORT}`);
+});
